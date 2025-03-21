9f16cac0981f50f783521ce9f3de8c9623e3e09b
kevlarpixels <kevingaguilar1@gmail.com>
2025-03-21 16:51:57 -0700

    Added link to Forms and updated place.json

--- src/BookingWidget.js
+++ src/BookingWidget.js
@@ -11,8 +11,8 @@ export default function BookingWidget({ place }) {
     return (
         <div className="bg-white shadow p-4 rounded-2xl">
             <div className="text-2xl text-center"></div>
-            Price: ${place.price} / per night
-            <div className="border rounded-2xl mt-4">
+            Rent: ${place.price} / a month
+           {/*  <div className="border rounded-2xl mt-4">
                 <div className="flex">
                     <div className="py-3 px-4">
                         <label>Check in:</label>
@@ -27,16 +27,16 @@ export default function BookingWidget({ place }) {
                             onChange={ev => setCheckOut(ev.target.value)} />
                     </div>
                 </div>
-                {/* <div className="py-3 px-4 border-t">
+                <div className="py-3 px-4 border-t">
                     <label>Number of guests:</label>
                     <input type="number"
                         value={numberOfGuests}
                         onChange={ev => setNumberOfGuests(ev.target.value)} />
-                </div> */}
+                </div> 
 
-            </div>
-            <button className="primary mt-4" onClick={() => window.location.href = "mailto:loangirlloanngirl@gmail.com"}>
-                Email Host
+            </div>*/}
+            <button className="primary mt-4" onClick={() => window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeXB8KfCSITQN4QJDYXxsm-ZJJ5l6DnKPnO1pmwReL13cOusw/viewform?usp=preview"}>
+                Fill out the application form
             </button>
         </div>
     )
